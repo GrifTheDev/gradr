3ea11925a80196169c957355036d8c986af79de8
GrifTheDev <70777013+GrifTheDev@users.noreply.github.com>
2023-05-29 18:45:50 +0200

    Login system without auth cookies!

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,30 +1,48 @@
-import type { Actions } from './$types'
-import { getDB } from '../../firebase'
-import { doc, getDoc } from 'firebase/firestore'
-import {sha256} from "js-sha256"
-
+import type { Actions } from "./$types";
+import { getDB } from "../../firebase";
+import { doc, getDoc } from "firebase/firestore";
+import { sha256 } from "js-sha256";
 
 export const actions = {
-    login: async ({cookies, request}) => {
-        const formData = await request.formData()
-        const email: string = formData.get('email')?.toString() || "empty"
-        const password: string = formData.get('password')?.toString() || "empty"
+  login: async ({ cookies, request }) => {
+    const formData = await request.formData();
+    const email: string = formData.get("email")?.toString() || "empty";
+    const password: string = formData.get("password")?.toString() || "empty";
 
-        // Data not submitted into one or more fields
-        if (email == "empty" || password == "empty") return { code: 500, message: "Please input data into both fields before clicking submit." }
-        // E-mail does not contain @something.com
-        if (email.split("@").length < 2) return { code: 500, message: "Invalid e-mail." }
-        // The part after @ in the email does not contain a .
-        if (email.split("@")[1].split(".").length < 2) return { code: 500, message: "Invalid e-mail." }
+    // Data not submitted into one or more fields
+    if (email == "empty" || password == "empty")
+      return {
+        code: 500,
+        message: "Please input data into both fields before clicking submit.",
+      };
+    // E-mail does not contain @something.com
+    if (email.split("@").length < 2)
+      return { code: 500, message: "Invalid e-mail." };
+    // The part after @ in the email does not contain a .
+    if (email.split("@")[1].split(".").length < 2)
+      return { code: 500, message: "Invalid e-mail." };
 
-        const sha256Email = sha256(email)
-        const { db } = getDB()
-        const docRef = doc(db, 'users', sha256Email)
-        const dbData = (await getDoc(docRef)).data()
+    const sha256Email = sha256(email);
+    const sha256Pswd = sha256(password);
+    const { db } = getDB();
+    const docRef = doc(db, "users", sha256Email);
+    const dbData = (await getDoc(docRef)).data();
 
-        if (dbData == undefined) return {code: 500, message: `Could not find an account registered with the email: ${email}`}
+    // For security reasons, it's better to say invalid username/password, than to mention what is wrong
+    if (dbData == undefined)
+      return {
+        code: 500,
+        message: `Invalid username/password.`,
+      };
 
-        console.log(dbData)
-        // now...
+    const dbPswd = dbData.password;
+
+    if (dbPswd == sha256Pswd) {
+      console.log("Logged in!");
+      
+    } else{
+        return { code: 500, message: "Invalid username/password." }
     }
-} satisfies Actions
+    // now...
+  },
+} satisfies Actions;

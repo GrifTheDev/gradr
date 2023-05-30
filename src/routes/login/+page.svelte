79b9a26be840ac5748f2a892da96a8c355ede180
GrifTheDev <70777013+GrifTheDev@users.noreply.github.com>
2023-05-30 18:03:05 +0200

    Small refactor

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -10,31 +10,20 @@
 <h1>Login</h1>
 
 {#if form?.message}
-  <div class="container">
-    <p>
-      ({form.code}) The server responded with an error: {form.message}
-    </p>
-    <form method="post" action="?/login">
-      E-mail: <input name="email" class="email-input" /> <br />
-      Password:
-      <input {type} autocomplete="off" name="password" class="pswd-input" />
-      <br />
-      <input type="checkbox" on:click={() => (showPswd = !showPswd)} />Show
-      Password <br />
-      <button type="submit">Submit</button>
-    </form>
-  </div>
-{:else}
-  <div class="container">
-    <form method="post" action="?/login">
-      E-mail: <input name="email" class="email-input" /> <br />
-      Password:
-      <input {type} autocomplete="off" name="password" class="pswd-input" />
-      <br />
-      <input type="checkbox" on:click={() => (showPswd = !showPswd)} />Show
-      Password <br />
-      <button type="submit">Submit</button>
-    </form>
-  </div>
+  <p>
+    ({form.code}) The server responded with an error: {form.message}
+  </p>
 {/if}
+
+<div class="container">
+  <form method="post" action="?/login">
+    E-mail: <input name="email" class="email-input" /> <br />
+    Password:
+    <input {type} autocomplete="off" name="password" class="pswd-input" />
+    <br />
+    <input type="checkbox" on:click={() => (showPswd = !showPswd)} />Show
+    Password <br />
+    <button type="submit">Submit</button>
+  </form>
+</div>
 <p>Don't have an account yet? <a href="/register">Register here.</a></p>
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -2,60 +2,35 @@
   import type { ActionData } from "./$types";
 
   export let form: ActionData;
-  let showPswd = false
+  let showPswd = false;
 
-  $: type = showPswd ? 'text' : 'password'
+  $: type = showPswd ? "text" : "password";
 </script>
 
 <h1>Register</h1>
 
 {#if form?.message}
-  <div class="container">
-    <p>
-      ({form.code}) The server responded with an error: {form.message}
-    </p>
-    <form method="post" action="?/register">
-      Username: <input name="username" class="username-input" /> <br />
-      E-mail: <input name="email" class="email-input" /> <br />
-      Password:
-      <input
-        type={ type }
-        autocomplete="off"
-        name="password"
-        class="pswd-input"
-      /> <br />
-      Confirm Password:
-      <input
-        type={ type }
-        autocomplete="off"
-        name="confirm-password"
-        class="pswd-input"
-      /> <br />
-      <input type="checkbox" on:click="{ () => showPswd = !showPswd }" />Show Password <br />
-      <button type="submit">Submit</button>
-    </form>
-  </div>
-{:else}
-  <div class="container">
-    <form method="post" action="?/register">
-      Username: <input name="username" class="username-input" /> <br />
-      E-mail: <input name="email" class="email-input" /> <br />
-      Password:
-      <input
-        type={ type }
-        autocomplete="off"
-        name="password"
-        class="pswd-input"
-      /> <br />
-      Confirm Password:
-      <input
-        type={ type }
-        autocomplete="off"
-        name="confirm-password"
-        class="pswd-input"
-      /> <br />
-      <input type="checkbox" on:click="{ () => showPswd = !showPswd }" />Show Password <br />
-      <button type="submit">Submit</button>
-    </form>
-  </div>
+  <p>
+    ({form.code}) The server responded with an error: {form.message}
+  </p>
 {/if}
+
+<div class="container">
+  <form method="post" action="?/register">
+    Username: <input name="username" class="username-input" /> <br />
+    E-mail: <input name="email" class="email-input" /> <br />
+    Password:
+    <input {type} autocomplete="off" name="password" class="pswd-input" />
+    <br />
+    Confirm Password:
+    <input
+      {type}
+      autocomplete="off"
+      name="confirm-password"
+      class="pswd-input"
+    /> <br />
+    <input type="checkbox" on:click={() => (showPswd = !showPswd)} />Show
+    Password <br />
+    <button type="submit">Submit</button>
+  </form>
+</div>

c774384c9158a6910410608d4565d0784681c598
GrifTheDev <70777013+GrifTheDev@users.noreply.github.com>
2023-05-30 12:52:19 +0200

    show password

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,6 +2,12 @@
   import type { ActionData } from "./$types";
 
   export let form: ActionData;
+  let showPswd = false
+
+  $: type = showPswd ? 'text' : 'password'
+
+  export function showPass() {
+  }
 </script>
 
 <h1>Login</h1>
@@ -9,20 +15,33 @@
 {#if form?.message}
   <div class="container">
     <p>
-      ({form.code}) The server responded with an error: {form.message} 
+      ({form.code}) The server responded with an error: {form.message}
     </p>
     <form method="post" action="?/login">
       E-mail: <input name="email" class="email-input" /> <br />
-      Password: <input type="password" autocomplete="off" name="password" class="pswd-input" /> <br />
+      Password:
+      <input
+        type={ type }
+        autocomplete="off"
+        name="password"
+        class="pswd-input"
+      /> <br />
+      <input type="checkbox" on:click="{ () => showPswd = !showPswd }" />Show Password <br />
       <button type="submit">Submit</button>
     </form>
-
   </div>
 {:else}
   <div class="container">
     <form method="post" action="?/login">
       E-mail: <input name="email" class="email-input" /> <br />
-      Password: <input type="password" autocomplete="off" name="password" class="pswd-input" /> <br />
+      Password:
+      <input
+        type={ type }
+        autocomplete="off"
+        name="password"
+        class="pswd-input"
+      /> <br />
+      <input type="checkbox" on:click="{ () => showPswd = !showPswd }" />Show Password <br />
       <button type="submit">Submit</button>
     </form>
   </div>
